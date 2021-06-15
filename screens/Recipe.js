810a7dc318cbd2ae382f04fd8fdbec58a8b4162a
Samson Nwokike <samson.nwokike@moduscreate.com>
2021-06-15 04:59:21 +0100

    initial-app-template

---
+++ screens/Recipe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Recipe = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Recipe</Text>
+    </View>
+  );
+};
+
+export default Recipe;
